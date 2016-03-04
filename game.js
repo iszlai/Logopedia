565cea57ac28c3c23129533e9378a0e671e686c4
iszlai <iszlai.lehel@gmail.com>
2016-03-04 18:29:28 +0100

    anmiations

--- game.js
+++ game.js
@@ -6,7 +6,8 @@ resources = PIXI.loader.resources,
 TextureCache = PIXI.utils.TextureCache,
 Texture = PIXI.Texture,
 Sprite = PIXI.Sprite;
-CARD_PNG="imgs/card.png";
+CARDA_PNG="imgs/card2.png";
+CARDB_PNG="imgs/card5.png";
 SCREEN_W=1024;
 SCREEN_H=768;
 charm = new Charm(PIXI);
@@ -19,12 +20,16 @@ document.body.appendChild(renderer.view);
 
 //load a JSON file and run the `setup` function when it's done
 loader
-.add(CARD_PNG)
+.add(CARDA_PNG)
+.add(CARDB_PNG)
 .load(setup);
 
 //Define variables that might be used in more
 //than one function
-var cardA,cardB;
+
+var cardA,cardB,sprite_animation,plier,card_to_filp;
+
+
 
 function setup() {
   cardA=getCard(1);
@@ -33,6 +38,7 @@ function setup() {
   stage.addChild(cardB);
   //Render the stage
   renderer.render(stage);
+
 }
 
 function gameLoop() {
@@ -41,7 +47,7 @@ function gameLoop() {
   requestAnimationFrame(gameLoop);
   charm.update();
 
-
+  flip(card_to_filp)
   //Render the stage to see the animation
   renderer.render(stage);
 }
@@ -56,15 +62,17 @@ function randomInt(min, max) {
 
 
 function getCard (side){
-  var card=new Sprite(resources[CARD_PNG].texture);
-  card.width=250;
-  card.height=200;
+  var card=new Sprite(resources[CARDA_PNG].texture);
+  card.otherside=resources[CARDB_PNG].texture;
+  card.isFlipped=false;
   var res= side ? getLEFTXY(card.width,card.height): getRightXY(card.width,card.height);
   card.x=res.x;
   card.y=res.y;
   card.interactive = true;
-  card.on('mousedown',onButtonDown);
-  card.on('touchstart', onButtonDown);
+  var cal=partial(onButtonDown,card)
+  card.on('mousedown',cal);
+  card.on('touchstart', cal);
+  console.log(card.scale.x)
   return card;
 }
 
@@ -87,20 +95,53 @@ function getRightXY(width,height){
   return res;
 }
 
-function onButtonDown(){
+function onButtonDown(p){
 
-  console.log("onButtonDown");
-  charm.breathe(
-    this,          //The sprite
-    scaleBy(this.scale.x,1.1),          //The final scale x value
-    scaleBy(this.scale.x,1.1),          //The final scale y value
-    60,             //The duration, in frames
-    false,               //Should the tween yoyo?
-    0  //Delay, in milliseconds, before yoyoing
-  );
-  //this.alpha = 0.5;
+if(!p.isFlipped){
+  sprite_animation=true;
+  plier=-1;
+  card_to_filp=p;
+  p.isFlipped=true;
+} else {
+  charm.scale(p,p.scale.x,scaleBy(p.scale.y,0.5),60);
+
+  charm.walkPath( p,  getWaypoints(p),  100,  "smoothstep",   false,  false,  200 );
+}
 }
 
 function scaleBy(current,factor){
-    return current*factor;
+  return current*factor;
+}
+
+function getWaypoints(p){
+  return [[p.x,p.y],[p.x,0],[SCREEN_W-p.width+80,0],[SCREEN_W-p.width+80,SCREEN_H-p.y-80]];
+}
+
+
+function flip(sprite){
+
+
+  if(sprite_animation){
+
+    if(plier > 0 && sprite.scale.x >0.83){
+      sprite_animation=false;
+    }
+
+    if(sprite.scale.x<0){
+      plier=1;
+      sprite.texture=sprite.otherside ;
+    }
+
+    sprite.scale.x+=plier*0.1;
+    //  sprite.y-=0.5;
+
+  }
+
+}
+function partial(func /*, 0..n args */) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  return function() {
+    var allArguments = args.concat(Array.prototype.slice.call(arguments));
+    return func.apply(this, allArguments);
+  };
 }
